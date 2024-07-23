d8061d86b503c5a2c7686db80b9932664ff5bc7d
sbhate2506 <60737815+sbhate2506@users.noreply.github.com>
2024-07-23 20:14:03 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv
+from summarize_video import summarize_video
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+@app.route("/summarize", methods=["POST"])
+def summarize():
+    url = request.form["url"]
+    summary = summarize_video(url=url)
+    return jsonify(
+        {
+            "summary" : summary.todict()        
+        }
+    )
---
+++ summarize_video.py
@@ -0,0 +1,7 @@
+from typing import Tuple
+def summarize_video(url:str)->Tuple[Summary Sentiments]:
+    #TODO
+    
+
+if __name__="__main__"
+    pass
