15415f5ca609f5d3a5579f0c87c7e2afc5737513
Shrikant <shrikant.bhate@hotmail.com>
2024-07-23 23:19:56 +0530

    added get comments method

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -25,7 +25,36 @@ def get_transcript(video_id):
     return output
     
 def get_comments(video_id):
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # DO NOT leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = os.getenv("YOUTUBE_API_KEY")
+    
+    yt = build(api_service_name, api_version, developerKy = DEVELOPER_KEY)
+    
+    video_response = yt.commentThreads().list(
+        part = 'snippet,replies',
+        videoId = video_id
+    ).execute()
+    
+    comments = []
+    while video_response:
+        for item in video_response["items"]:
+            comments.append(item['snippet']['topLevelComment']['snippet']['textDisplay'])
+            
+        if 'nextPageToken' in video_response:
+            video_response = yt.commentThreads().list(
+                part = 'snippet,replies',
+                videoId = video_id
+                pageToken = video_response['nextPageToken']            
+            ).execute()
+        else:
+            break
+            
+    return comments
     
 
 
